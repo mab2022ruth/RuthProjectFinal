ef4fd4d30c9852f87176ab305400dc4d45932e01
ruth.ngolaminga <ruth.ngolaminga@IC-CS233-24.uc.ucalgary.ca>
2022-08-12 14:10:32 -0400

    Removed stylesheet and added title to application

--- src/application/Main.java
+++ src/application/Main.java
@@ -14,6 +14,8 @@ public class Main extends Application {
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Ruth Mab's Project");
+
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
