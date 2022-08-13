5ecb624a31d2e74c2786a085fcf055a1b23c9111
ruth.ngolaminga <ruth.ngolaminga@IC-CS233-9.uc.ucalgary.ca>
2022-08-13 14:24:31 -0400

    Created an abstract bank account

---
+++ src/application/BankAccount.java
@@ -0,0 +1,45 @@
+package application;
+
+abstract public class BankAccount {
+	int balance;
+	int previousTransaction;
+	public static String customerName;
+	public static String customerId;
+	
+	BankAccount(String name, String id)
+	 {
+		 customerName = name;
+		 customerId = id;
+	 }
+	
+	public abstract void deposit(int amount);
+	
+	
+	public abstract void withdraw(int amount);
+	
+	
+	
+	void getPreviousTransaction()
+	{
+		if(previousTransaction > 0)
+		{
+			
+			System.out.println("Deposited: " + previousTransaction);
+
+		}
+		else if(previousTransaction <0)
+		{
+			System.out.println("Withdrawn"  + Math.abs(previousTransaction));
+		}
+		else
+		{
+			System.out.println("No transaction occured");
+
+		}
+		
+	}
+	
+	
+
+}
+
