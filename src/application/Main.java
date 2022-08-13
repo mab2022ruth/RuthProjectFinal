c3648e08ff6628afd0f8b03fd35d72334cf8ad38
ruth.ngolaminga <ruth.ngolaminga@IC-CS233-9.uc.ucalgary.ca>
2022-08-13 16:13:26 -0400

    First step works. the fxml file was linked to the main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,18 +1,22 @@
 package application;
 	
+import java.io.FileInputStream;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			FXMLLoader loader = new FXMLLoader();
+			VBox root = loader.load(new FileInputStream("src/application/BankAccountView.fxml"));
 			Scene scene = new Scene(root,400,400);
-		//	scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Ruth' mab Bank Account Application");
 
