3f1bac6f044c4608dce4ccc038b2d84c7101cd29
ruth.ngolaminga <ruth.ngolaminga@IC-CS233-43.uc.ucalgary.ca>
2022-08-13 20:37:38 -0400

    updated the files

--- src/application/BankAccountController.java
+++ src/application/BankAccountController.java
@@ -2,9 +2,12 @@ package application;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 
 public class BankAccountController {
+	 @FXML
+	 private ChoiceBox<Integer> choiceBoxDepositWithdraw;
 
     @FXML
     private TextField nameOfCustomer;
@@ -20,12 +23,14 @@ public class BankAccountController {
     	String accountEntered = accountNumber.getText();
     	//System.out.println(nameEntered );
     	//System.out.println(accountEntered);
+    	int choiceEntered = choiceBoxDepositWithdraw.getValue();
     	
     	
     	Checking customerIn = new Checking(nameEntered, accountEntered);
     	
     	System.out.println("This is the customer's name: " +customerIn.customerName );
     	System.out.println("This is the customer's id: "+ customerIn.customerId );
+    	System.out.println("This is the customer's choice value: "+ choiceEntered);
 
 
 
